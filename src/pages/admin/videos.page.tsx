8b4154bd6f814abd422f99c3023ccc2e9228d3d5
Dinh Duong <jenny.180820@gmail.com>
2025-07-07 22:08:44 +0700

    feat(video): fetch all videos
    Closes: #47

--- src/pages/admin/videos.page.tsx
+++ src/pages/admin/videos.page.tsx
@@ -1,16 +1,26 @@
-import { Button, Card, Col, notification, Row, Space, Spin, Tree } from "antd";
+import { Button, Card, Col, notification, Popconfirm, Row, Space, Spin, Tree } from "antd";
 import type { DataNode } from "antd/es/tree";
 import { useEffect, useRef, useState, type Key } from "react";
 import type { ICategory } from "types/category.type";
 import { fetchCategoriesAPI } from "services/category.service";
 import type { IconType } from "antd/es/notification/interface";
-import type { ActionType } from "@ant-design/pro-components";
-import { EditOutlined, PlusOutlined } from "@ant-design/icons";
+import { ProTable, type ActionType, type ProColumns } from "@ant-design/pro-components";
+import { DeleteOutlined, EditOutlined, PlusOutlined, QuestionCircleOutlined } from "@ant-design/icons";
 import CreateCategoryModal from "components/category/create-category-modal.component";
 import UpdateCategoryModal from "components/category/update-category-modal.component";
+import type { IVideo } from "types/video.type";
+import type { IMeta } from "types/backend";
+import { formatISODate } from "utils/format.util";
+import { fetchVideosAPI } from "services/video.service";
 
 const VideoPage = () => {
     const actionRef = useRef<ActionType>(null);
+    const [meta, setMeta] = useState<IMeta>({
+        page: 1,
+        pageSize: 10,
+        pages: 1,
+        total: 0
+    });
     const [isCategoryModalOpen, setIsCategoryModalOpen] = useState(false);
     const [selectedCategoryId, setSelectedCategoryId] = useState<number | null>(null);
     const [categories, setCategories] = useState<DataNode[]>([]);
@@ -93,6 +103,97 @@ const VideoPage = () => {
         fetchCategories();
     };
 
+    const columns: ProColumns<IVideo>[] = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+            search: false,
+        },
+        {
+            title: 'Title',
+            dataIndex: 'title',
+            key: 'title',
+            ellipsis: true,
+            sorter: true,
+            render: (_, record) => (
+                <a onClick={() => ''}>
+                    {record.title}
+                </a>
+            )
+        },
+        {
+            title: 'Duration',
+            dataIndex: 'duration',
+            key: 'duration',
+            sorter: true,
+            hideInSearch: true,
+            render: (_, record) => (
+                <>
+                    {record.duration}
+                </>
+            )
+        },
+        {
+            title: 'Created at',
+            dataIndex: 'createdAt',
+            key: 'createdAt',
+            sorter: true,
+            hideInSearch: true,
+            render: (value) => {
+                return formatISODate(value?.toString() || '');
+            },
+        },
+        {
+            title: 'Created at',
+            dataIndex: 'createdAt',
+            valueType: 'dateRange',
+            hideInTable: true,
+            search: {
+                transform: (value) => {
+                    return {
+                        startCreatedAt: value[0],
+                        endCreatedAt: value[1],
+                    };
+                },
+            },
+        },
+        {
+            title: 'Updated at',
+            dataIndex: 'updatedAt',
+            key: 'updatedAt',
+            sorter: true,
+            hideInSearch: true,
+            render: (value) => {
+                return formatISODate(value?.toString() || '');
+            },
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            search: false,
+            render: (_, record) => (
+                <Space size="middle">
+                    <Button icon={<EditOutlined />} color="primary"
+                        onClick={() => ''}>
+                    </Button>
+                    <Popconfirm
+                        title="Delete the article"
+                        description={`Are you sure to delete video": ${record.title}?`}
+                        onConfirm={() => ''}
+                        icon={<QuestionCircleOutlined />}
+                        okText="Yes"
+                        cancelText="No"
+                        placement="leftTop"
+                    >
+                        <Button icon={<DeleteOutlined />} danger>
+                        </Button>
+                    </Popconfirm>
+                </Space>
+            ),
+        },
+    ];
+
     return (
         <>
             <Row gutter={[16, 16]}>
@@ -109,7 +210,80 @@ const VideoPage = () => {
                     </Card>
                 </Col>
                 <Col span={18}>
+                    {selectedCategoryId ? (
+                        <ProTable<IVideo>
+                            columns={columns}
+                            actionRef={actionRef}
+                            request={async (params, sort, filter) => {
+                                const queryParts: string[] = [];
+                                queryParts.push(`categoryId=${selectedCategoryId}`);
+                                queryParts.push(`page=${params.current}`);
+                                queryParts.push(`size=${params.pageSize}`);
 
+                                for (const key in filter) {
+                                    if (filter[key]) {
+                                        queryParts.push(`${key}=${filter[key]}`);
+                                    }
+                                }
+
+                                const searchParams = { ...params };
+                                delete searchParams.current;
+                                delete searchParams.pageSize;
+
+                                for (const key in searchParams) {
+                                    if (searchParams[key]) {
+                                        queryParts.push(`${key}=${searchParams[key]}`);
+                                    }
+                                }
+
+                                if (sort) {
+                                    for (const key in sort) {
+                                        const value = sort[key];
+                                        queryParts.push(`sort=${key},${value === 'ascend' ? 'asc' : 'desc'}`);
+                                    }
+                                }
+
+                                const query = queryParts.join('&');
+
+                                const res = await fetchVideosAPI(query);
+
+                                if (res && res.data) {
+                                    setMeta(res.data.meta);
+                                    return {
+                                        data: res.data.result,
+                                        page: 1,
+                                        success: true,
+                                        total: res.data.meta.total,
+                                    };
+                                } else {
+                                    return {
+                                        data: [],
+                                        success: false,
+                                        total: 0,
+                                    };
+                                }
+                            }}
+                            rowKey="id"
+                            pagination={{
+                                current: meta.page,
+                                pageSize: meta.pageSize,
+                                showSizeChanger: true,
+                                total: meta.total
+                            }}
+                            toolBarRender={() => [
+                                <Button type="primary" key="primary"
+                                    icon={<PlusOutlined />}
+                                    onClick={() => ''}
+                                >
+                                    Create
+                                </Button>,
+                            ]}
+                            scroll={{ x: 'max-content' }}
+                            headerTitle="Article Management"
+                        />
+                    ) : (
+                        <Card><p>Please select a category to view videos.</p></Card>
+                    )}
                 </Col>
             </Row>
 
--- src/services/video.service.ts
+++ src/services/video.service.ts
@@ -2,7 +2,7 @@ import type { IBackendRes } from "types/backend";
 import type { IVideo } from "types/video.type";
 import instance from "services/axios.customize";
 
-export const fetchArticlesAPI = (query: string) => {
+export const fetchVideosAPI = (query: string) => {
     const url_backend = `/api/v1/admin/videos?${query}`;
     return instance.get<IBackendRes<IVideo[]>>(url_backend);
 }
