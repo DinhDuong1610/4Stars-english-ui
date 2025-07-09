4fe4c84deb0d067454c67bc0450db1c4b00b225a
Dinh Duong <jenny.180820@gmail.com>
2025-07-09 15:02:00 +0700

    feat(grammar): fetch all grammars
    Closes: #57

--- src/pages/admin/grammars.page.tsx
+++ src/pages/admin/grammars.page.tsx
@@ -1,14 +1,17 @@
-import type { ActionType } from "@ant-design/pro-components";
+import { ProTable, type ActionType, type ProColumns } from "@ant-design/pro-components";
 import { useEffect, useRef, useState, type Key } from "react";
 import type { IMeta } from "types/backend";
 import type { DataNode } from "antd/es/tree";
-import { Button, Card, Col, notification, Row, Space, Spin, Tree } from "antd";
+import { Button, Card, Col, notification, Popconfirm, Row, Space, Spin, Tree } from "antd";
 import type { IconType } from "antd/es/notification/interface";
 import { fetchCategoriesAPI } from "services/category.service";
 import type { ICategory } from "types/category.type";
-import { EditOutlined, PlusOutlined } from "@ant-design/icons";
+import { DeleteOutlined, EditOutlined, PlusOutlined, QuestionCircleOutlined } from "@ant-design/icons";
 import CreateCategoryModal from "components/category/create-category-modal.component";
 import UpdateCategoryModal from "components/category/update-category-modal.component";
+import type { IGrammar } from "../../types/grammar.type";
+import { formatISODate } from "../../utils/format.util";
+import { fetchGrammarsAPI } from "../../services/grammar.service";
 
 const GrammarPage = () => {
     const actionRef = useRef<ActionType>(null);
@@ -101,6 +104,86 @@ const GrammarPage = () => {
         fetchCategories();
     };
 
+    const columns: ProColumns<IGrammar>[] = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+            search: false,
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            ellipsis: true,
+            sorter: true,
+            render: (_, record) => (
+                <a onClick={() => ''}>
+                    {record.name}
+                </a>
+            )
+        },
+        {
+            title: 'Created at',
+            dataIndex: 'createdAt',
+            key: 'createdAt',
+            sorter: true,
+            hideInSearch: true,
+            render: (value) => {
+                return formatISODate(value?.toString() || '');
+            },
+        },
+        {
+            title: 'Created at',
+            dataIndex: 'createdAt',
+            valueType: 'dateRange',
+            hideInTable: true,
+            search: {
+                transform: (value) => {
+                    return {
+                        startCreatedAt: value[0],
+                        endCreatedAt: value[1],
+                    };
+                },
+            },
+        },
+        {
+            title: 'Updated at',
+            dataIndex: 'updatedAt',
+            key: 'updatedAt',
+            sorter: true,
+            hideInSearch: true,
+            render: (value) => {
+                return formatISODate(value?.toString() || '');
+            },
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            search: false,
+            render: (_, record) => (
+                <Space size="middle">
+                    <Button icon={<EditOutlined />} color="primary"
+                        onClick={() => ''}>
+                    </Button>
+                    <Popconfirm
+                        title="Delete the article"
+                        description={`Are you sure to delete article": ${record.name}?`}
+                        onConfirm={() => 'handleDeleteUser(record.id)'}
+                        icon={<QuestionCircleOutlined />}
+                        okText="Yes"
+                        cancelText="No"
+                        placement="leftTop"
+                    >
+                        <Button icon={<DeleteOutlined />} danger>
+                        </Button>
+                    </Popconfirm>
+                </Space>
+            ),
+        },
+    ];
+
+
     return (
         <>
             <Row gutter={[16, 16]}>
@@ -118,7 +201,76 @@ const GrammarPage = () => {
                 </Col>
                 <Col span={18}>
                     {selectedCategoryId ? (
-                        <></>
+                        <ProTable<IGrammar>
+                            columns={columns}
+                            actionRef={actionRef}
+                            request={async (params, sort, filter) => {
+                                const queryParts: string[] = [];
+                                queryParts.push(`categoryId=${selectedCategoryId}`);
+                                queryParts.push(`page=${params.current}`);
+                                queryParts.push(`size=${params.pageSize}`);
+
+                                for (const key in filter) {
+                                    if (filter[key]) {
+                                        queryParts.push(`${key}=${filter[key]}`);
+                                    }
+                                }
+
+                                const searchParams = { ...params };
+                                delete searchParams.current;
+                                delete searchParams.pageSize;
+
+                                for (const key in searchParams) {
+                                    if (searchParams[key]) {
+                                        queryParts.push(`${key}=${searchParams[key]}`);
+                                    }
+                                }
+
+                                if (sort) {
+                                    for (const key in sort) {
+                                        const value = sort[key];
+                                        queryParts.push(`sort=${key},${value === 'ascend' ? 'asc' : 'desc'}`);
+                                    }
+                                }
+
+                                const query = queryParts.join('&');
+
+                                const res = await fetchGrammarsAPI(query);
+
+                                if (res && res.data) {
+                                    setMeta(res.data.meta);
+                                    return {
+                                        data: res.data.result,
+                                        page: 1,
+                                        success: true,
+                                        total: res.data.meta.total,
+                                    };
+                                } else {
+                                    return {
+                                        data: [],
+                                        success: false,
+                                        total: 0,
+                                    };
+                                }
+                            }}
+                            rowKey="id"
+                            pagination={{
+                                current: meta.page,
+                                pageSize: meta.pageSize,
+                                showSizeChanger: true,
+                                total: meta.total
+                            }}
+                            toolBarRender={() => [
+                                <Button type="primary" key="primary"
+                                    icon={<PlusOutlined />}
+                                    onClick={() => ''}
+                                >
+                                    Create
+                                </Button>,
+                            ]}
+                            scroll={{ x: 'max-content' }}
+                            headerTitle="Grammar Management"
+                        />
                     ) : (
                         <Card><p>Please select a category to view articles.</p></Card>
                     )}
